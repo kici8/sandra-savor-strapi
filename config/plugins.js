113461d05402a2ab068fd32f6fd522a92b7a4c03
kici8 <artusi.daniele@gmail.com>
2023-12-31 00:16:45 +0100

    feat: show graphql playground on production

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,32 @@
 module.exports = ({ env }) => ({
-  'users-permissions': {
+  "users-permissions": {
     config: {
-      jwtSecret: env('JWT_SECRET'),
+      jwtSecret: env("JWT_SECRET"),
     },
   },
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
         delete: {},
+      },
+    },
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
       },
     },
   },
